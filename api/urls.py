d0e45ea5d93be521a246c01a43197cf6ba81e0d8
f3ve <free.eve@pm.me>
2021-01-05 14:27:51 -0800

    added a new url path for fetching categories

--- api/urls.py
+++ api/urls.py
@@ -5,10 +5,12 @@ custom urls
 from django.urls import path
 from api.user import views as userViews
 from api.article import views as articleViews
+from api.category import views as categoryViews
 
 urlpatterns = [
     path("users/", userViews.UsersView.as_view(), name="users"),
     path("users/<pk>/", userViews.UserDetailView.as_view(), name="user"),
     path("articles/", articleViews.ArticlesView.as_view(), name="articles"),
-    path("articles/<pk>/", articleViews.ArticleDetailView.as_view(), name="article")
+    path("articles/<pk>/", articleViews.ArticleDetailView.as_view(), name="article"),
+    path("categories/", categoryViews.CategoryView.as_view, name="categories")
 ]
